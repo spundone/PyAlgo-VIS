7b24af4bf6698fbca057cd5997060a74aca6c433
Spandan Ghosh <spandu500@gmail.com>
2021-02-17 17:54:24 +0530

    Add Algorithms

---
+++ algorithms/__init__.py
@@ -0,0 +1,36 @@
+from algorithms.bogoSort import bogoSort
+from algorithms.bubbleSort import bubbleSort
+from algorithms.cocktailSort import cocktailSort
+from algorithms.countingSort import countingSort
+from algorithms.cycleSort import cycleSort
+from algorithms.gnomeSort import gnomeSort
+from algorithms.heapSort import heapSort
+from algorithms.insertionSort import insertionSort
+from algorithms.mergeSort import mergeSort
+from algorithms.quickSort import quickSort
+from algorithms.selectionSort import selectionSort
+from algorithms.radixSort import radixSort
+from algorithms.shellSort import shellSort
+from algorithms.combSort import combSort
+from algorithms.bucketSort import bucketSort
+from algorithms.strandSort import strandSort
+
+
+__all__ = [
+    "bogoSort",
+    "bubbleSort",
+    "cocktailSort",
+    "countingSort",
+    "cycleSort",
+    "gnomeSort",
+    "heapSort",
+    "insertionSort",
+    "mergeSort",
+    "quickSort",
+    "selectionSort",
+    "radixSort",
+    "shellSort",
+    "combSort",
+    "bucketSort",
+    "strandSort"
+]
