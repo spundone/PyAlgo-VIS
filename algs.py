86b9b2842500c44f83766164765b4d36ae89f346
Spandan Ghosh <spandu500@gmail.com>
2021-03-03 17:44:45 +0530

    Debloat

--- algs.py
+++ algs.py
@@ -1,10 +1,5 @@
 from algorithms import *
 from algorithms.binaryinsertionSort import binaryinsertionSort
-from algorithms.bitonicSort import bitonicSort
-from algorithms.pancakeSort import pancakeSort
-from algorithms.timSort import timSort
-from algorithms.stoogeSort import stoogeSort
-from algorithms.strandSort import strandSort
 
 algorithmsDict = {
     'insertionsort': insertionSort,
@@ -12,22 +7,10 @@ algorithmsDict = {
     'selectionsort': selectionSort,
     'mergesort': mergeSort,
     'quicksort': quickSort,
-    'countingsort': countingSort,
-    'cocktailsort': cocktailSort,
-    'cyclesort': cycleSort,
     'bogosort': bogoSort,
     'heapsort': heapSort,
     'radixsort': radixSort,
-    'shellsort': shellSort,
-    'gnomesort': gnomeSort,
-    'combsort': combSort,
-    'bitonicsort': bitonicSort,
-    'pancakesort': pancakeSort,
     'binaryinsertionsort': binaryinsertionSort,
-    'bucketsort': bucketSort,
-    'timsort' :timSort,
-    'stoogesort': stoogeSort,
-    'strandsort':strandSort
 }
 
 
