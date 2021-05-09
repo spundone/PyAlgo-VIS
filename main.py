e50bd9bcd17496e98bc2ac64871009160193f97c
Spandan Ghosh <spandu500@gmail.com>
2021-05-09 07:52:48 +0530

    add speed and size sliders

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from tkinter import ttk
 import random
 from colors import *
 
-# Importing algorithms 
+# Importing algorithms
 from algorithms.bubbleSort import bubble_sort
 from algorithms.selectionSort import selection_sort
 from algorithms.insertionSort import insertion_sort
@@ -13,21 +13,39 @@ from algorithms.heapSort import heap_sort
 from algorithms.countingSort import counting_sort
 
 
-# Main window 
+# Main window
 window = Tk()
 window.title("Sorting Algorithms Visualization")
 window.maxsize(1000, 700)
-window.config(bg = WHITE)
+window.config(bg=WHITE)
 
-
+maxval = DoubleVar()
+speed = DoubleVar()
 algorithm_name = StringVar()
 speed_name = StringVar()
 data = []
-algo_list = ['Bubble Sort', 'Insertion Sort', 'Selection Sort', 'Merge Sort', 'Quick Sort', 'Heap Sort', 'Counting Sort']
-speed_list = ['Fast', 'Medium', 'Slow']
+algo_list = ['Bubble Sort', 'Insertion Sort', 'Selection Sort',
+             'Merge Sort', 'Quick Sort', 'Heap Sort', 'Counting Sort']
+s = Scale(window, label='Size of Array', variable=maxval,
+          orient=HORIZONTAL, length=250, from_=10, to=500, bg=WHITE)
+s.place(x=20, y=15)
+s = Scale(window, label='speed', variable=speed, orient=HORIZONTAL,
+          resolution=0.001, length=250, from_=0.001, to=3, bg=WHITE)
+s.place(x=550, y=15)
 
+# # components in launch_test window
+# instruct_label = Label(test_page, text="Choose Three Algorithms:")
+# run_btn = Button(test_page, text="Run", command=run)
+# launch_s = Scale(test_page, orient=HORIZONTAL, length=125, from_=c, to=d)
+# launch_s.place(x=300, y=16)
+
+# # layout for launch_test_windows
+# run_btn.place(x=450, y=32)
+# instruct_label.place(x=48, y=36)
 
 # Drawing the numerical array as bars
+
+
 def drawData(data, colorArray):
     canvas.delete("all")
     canvas_width = 800
@@ -52,26 +70,17 @@ def generate():
     global data
 
     data = []
-    for i in range(0, 100):
-        random_value = random.randint(1, 150)
+    for i in range(0, int(maxval.get())):
+        random_value = random.randint(1, 250)
         data.append(random_value)
 
     drawData(data, [BLUE for x in range(len(data))])
 
 
-def set_speed():
-    if speed_menu.get() == 'Slow':
-        return 0.3
-    elif speed_menu.get() == 'Medium':
-        return 0.1
-    else:
-        return 0.001
-
-
 def sort():
     global data
-    timeTick = set_speed()
-    
+    timeTick = speed.get()
+
     if algo_menu.get() == 'Bubble Sort':
         bubble_sort(data, drawData, timeTick)
     elif algo_menu.get() == 'Selection Sort':
@@ -89,20 +98,16 @@ def sort():
 
 
 ### User interface ###
-UI_frame = Frame(window, width= 900, height=300, bg=WHITE)
+UI_frame = Frame(window, width=900, height=300, bg=WHITE)
 UI_frame.grid(row=0, column=0, padx=10, pady=5)
 
 l1 = Label(UI_frame, text="Algorithm: ", bg=WHITE)
 l1.grid(row=0, column=0, padx=10, pady=5, sticky=W)
-algo_menu = ttk.Combobox(UI_frame, textvariable=algorithm_name, values=algo_list)
+algo_menu = ttk.Combobox(
+    UI_frame, textvariable=algorithm_name, values=algo_list)
 algo_menu.grid(row=0, column=1, padx=5, pady=5)
 algo_menu.current(0)
 
-l2 = Label(UI_frame, text="Sorting Speed: ", bg=WHITE)
-l2.grid(row=1, column=0, padx=10, pady=5, sticky=W)
-speed_menu = ttk.Combobox(UI_frame, textvariable=speed_name, values=speed_list)
-speed_menu.grid(row=1, column=1, padx=5, pady=5)
-speed_menu.current(0)
 
 canvas = Canvas(window, width=800, height=400, bg=WHITE)
 canvas.grid(row=1, column=0, padx=10, pady=5)
